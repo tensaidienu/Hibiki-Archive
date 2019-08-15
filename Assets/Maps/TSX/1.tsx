43b75817af14c4e31c6f9e084f97809b09435259
TensaiDN <tensaidienu@gmail.com>
2019-08-15 01:14:45 -0300

    Assets, Build and WebBuild
    Assets, Build and WebBuild

---
+++ Assets/Maps/TSX/1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="1" tilewidth="32" tileheight="32" tilecount="144" columns="8">
+ <image source="../Tilesets/1.bmp" trans="f800f8" width="256" height="576"/>
+</tileset>
---
+++ Assets/Maps/TSX/country-platform-back.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="country-platform-back" tilewidth="32" tileheight="32" tilecount="84" columns="12">
+ <image source="../Tilesets/country-platform-back.png" width="384" height="224"/>
+</tileset>
